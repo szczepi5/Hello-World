0df0160fa1cabad4d016d6ec9e0b49c124b4ff17
szczepi5 <mateusz.szczepkowski450@gmail.com>
2017-03-04 12:36:08 +0100

    add files

---
+++ src/com/sdajava/zad1/Main.java
@@ -0,0 +1,175 @@
+package com.sdajava.zad1;
+
+
+import jdk.nashorn.internal.parser.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        char znak = 'k';
+        int a=23, b=43, wynik=0;
+        System.out.printf("Zmienna:" + (int) znak);
+
+        Scanner wejscie=new Scanner(System,in);
+        System.out.printf("Podaj znak: ");
+
+        switch (znak)
+        {
+            case (znak)
+        }
+    }
+}
+
+
+
+//        int a = 764466;
+//
+//        if (a % 2 == 0) {
+//            System.out.printf("Podzielna");
+//        } else {
+ //           System.out.printf("Nie");
+//        }
+//    }
+//}
+
+
+
+//        int a=1;
+//        int b=2;
+//        int c=3;
+//       int x=(c-b)/a;
+//
+//        if(a == 0){
+//            System.out.printf("");
+//        }else{
+//            System.out.printf("Wynik: " + x);
+//        }
+
+
+
+
+
+
+//     double a=2;
+//        double b=3;
+//        double c=4;
+//
+//        if ( (a*a)+(b*b)==(c*c))
+ //       {
+//            System.out.println("Jest");
+//
+ //       }
+ //       else {
+ //           System.out.println("Nie jest");
+ //       }
+
+
+
+//        int a = 4;
+//       int b = -2;
+//        int c = 10;
+//        int d,x1,x2;
+
+
+//        d=b*b-4*a*c;
+
+
+ //       if (d == 0)
+//            {
+ //           x1 = (-b / (2 * a));
+
+ //               System.out.println(x1);
+ //   }
+ //       else if (d < 0)        {
+   //         {
+   //         System.out.println("brak rozw");
+   //     }
+
+    //    else if (d >0)
+    //        {
+    //            x1= (-b + Math.sqrt(d) / (2*a);
+    //            x2= (-b -  Math.sqrt(d) / (2*a);
+    //            System.out.println("ok");
+     //       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Main {
+//    public static void main(String[] args) {
+
+//        int a = 32341;
+ //       int b = 233;
+//
+ //       if (a % b == 0){
+ //
+ //       }else{
+ //
+ //       }
+//
+ //   }
+//}
+
+//double liczba = 43.43543646;
+//if (liczba % 1) == 0)
+//        {
+//        System.out.println("stało przecinkowa");
+//        }
+//math.round();
+//        System.out.format ("%.2ff%n, a);
+
+
+
+ //   public static void main(String[] args) {
+ //       System.out.println("Rammstein");
+ //      int x = 10;
+ //      int y = 20;
+ //      int z = 30;
+
+  //     if (z == x+y) {
+ //           System.out.println("zgadza się");
+ //      } else {
+  //          System.out.println("nie zgadza się");
+ //      }
+ //   }
+//}
+
+
+//        int x = 10;
+//        int y = 20;
+//        int a = x * 2;
+
+//        if (y == a) {
+//            System.out.println("zgadza się");
+//        } else {
+//            System.out.println("nie zgadza się");
+//        }
+//    }
+//}
+
+
+
+    //    int a=180; //waga
+    //    int b=160;//wzrost
+    //    int c=a/b*b;
+
+    //    if (c>=18.5 && c<=24.9)
+    //    {
+    //     System.out.println("waga prawidłowa");
+    //    }
+    //    else if(c<18.5)
+    //    {
+    //        System.out.println("waga za niska");
+    //    }
+     //   else{
+    //    System.out.println("waga za wysoka");    }
+//}
+//}
